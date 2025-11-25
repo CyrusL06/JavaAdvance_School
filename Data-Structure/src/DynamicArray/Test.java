be21097e30c5b68bede7404ed4a7cda080c731f7
cyruslorenzo <cyruslorenzo0626@gmail.com>
2025-11-24 22:07:53 -0800

    Fixed the Structure

---
+++ Data-Structure/src/DynamicArray/Test.java
@@ -0,0 +1,31 @@
+package DynamicArray;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+//		ArrayList<String> arrayList = new ArrayList<trings>();
+		
+		DynamicArray dynamicArray = new DynamicArray();
+		System.out.println(dynamicArray.capacity);
+		
+//		
+		dynamicArray.add("A");
+		dynamicArray.add("B");
+		dynamicArray.add("C");
+		
+		dynamicArray.insert(0,"X");
+
+		
+		System.out.println("size: " + dynamicArray.size);
+		System.out.println("Capacity: " + dynamicArray.capacity);
+		
+		System.out.println(dynamicArray);
+
+		
+		System.out.println( "empty: "  + dynamicArray.isEmpty() );
+
+	}
+
+}
